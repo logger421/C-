2f3885514c769687ac77425760d94144d788cfef
Wojtek Lotko <wojteklotko3@gmail.com>
2022-04-09 23:17:09 +0200

    Update main.cpp src code

---
+++ CreditCardValidation/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+const std::string EXIT = "quit";
+
+bool is_number(const std::string toCheck);
+
+int main() {
+    std::cout << "Welcome to check if my credit card number is valid program!\n";
+    std::cout << "Please enter your credit card number:(or type \"quit\" to exit) ";
+    std::string credit_card_number;
+    std::cin >> credit_card_number;
+
+    while(true) {
+
+        if(credit_card_number == EXIT) {
+            std::cout << "Exiting from program...\n";
+            break;
+        }
+
+        if(!is_number(credit_card_number)) {
+            std::cout << "Given number is not a credit card number\n";
+            std::cout << "please enter a number: ";
+            std::cin >> credit_card_number;
+            continue;
+        }
+        
+    }
+    std::cout << "See you next time!\n";
+}
+
+bool is_number(const std::string toCheck) {
+    return false;
+}
